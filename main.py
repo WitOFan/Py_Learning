6245da56d438265d545d6b696ba251e2ebef5403
WitOFan <andru31ru@yandex.ru>
2024-12-25 19:00:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st program
+print (9**0.5 * 5)
+
+#2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+a = 2*2+2
+b = 2*(2+2)
+print (a)
+print (b)
+print (a == b)
+
+#4th program
+st = "123.456"
+print (type(st))
+float_st = float(st)
+print (int(float_st*10%10))
+
